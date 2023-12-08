ee00786e6912410cdba9d6a3b86cbf19342ac154
Max <60605508+karuzoXam@users.noreply.github.com>
2023-12-08 18:13:20 +0100

    Setup Global Styles

--- FirstReactApp/src/components/Info.jsx
+++ FirstReactApp/src/components/Info.jsx
@@ -1,7 +1,13 @@
+import profileImage from '../images/profile_pic.png';
+
 function Info() {
   return (
     <div>
-      <p>Photo, name, buttons</p>
+      <img src={profileImage} alt="Profile picture" width="317px" height="317px"></img>
+      <h1>Maximilian Stadlmeyer</h1>
+      <h2>Frontend Developer</h2>
+      <a>maxstadlmeyer.dev</a>
+      <a>Email</a>
     </div>
   );
 }

d9a48d1d2117378c84c994ed6344ca63fdcc89be
Max <60605508+karuzoXam@users.noreply.github.com>
2023-12-18 14:43:13 +0100

    style for about.css&Interests.css

--- FirstReactApp/src/components/About.jsx
+++ FirstReactApp/src/components/About.jsx
@@ -1,4 +1,16 @@
+import './About.css';
+
 function MainContent() {
-  return <h1>About</h1>;
+  return (
+    <div className="about-container">
+      <h1 className="about">About</h1>
+
+      <p className="about-text">
+        I am a frontend developer with a particular interest in making things simple and automating
+        daily tasks. I try to keep up with security and best practices, and am always looking for
+        new things to learn.
+      </p>
+    </div>
+  );
 }
 export default MainContent;
--- FirstReactApp/src/components/Interests.jsx
+++ FirstReactApp/src/components/Interests.jsx
@@ -1,4 +1,16 @@
+import './Interests.css';
+
 function Interests() {
-  return <h1>Interests</h1>;
+  return (
+    <div className="interests-container">
+      <h1 className="interests">Interests</h1>
+
+      <p className="interests-text">
+        I am a frontend developer with a particular interest in making things simple and automating
+        daily tasks. I try to keep up with security and best practices, and am always looking for
+        new things to learn.
+      </p>
+    </div>
+  );
 }
 export default Interests;

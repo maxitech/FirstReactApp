40d422b2a51a13166a22e2ab654f9d9cdb3d7182
Max <60605508+karuzoXam@users.noreply.github.com>
2023-12-18 11:51:25 +0100

    formatting info component

--- FirstReactApp/src/components/Info.jsx
+++ FirstReactApp/src/components/Info.jsx
@@ -14,12 +14,15 @@ function Info() {
         width="317px"
         height="317px"
       ></img>
+
       <div className="contact-container">
         <h1 className="name">Maximilian Stadlmeyer</h1>
         <h2 className="job">Frontend Developer</h2>
+
         <a className="website-link" href="#">
           maxstadlmeyer.dev
         </a>
+
         <a className="email-link" href="#">
           <div className="email-btn">
             <FontAwesomeIcon className="email-icon" icon={faEnvelope} />

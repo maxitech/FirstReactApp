fdb5cef8a481bc5d7b514478d6df30526ed853b5
Max <60605508+karuzoXam@users.noreply.github.com>
2023-12-18 11:30:14 +0100

    styling for info component

--- FirstReactApp/src/components/Info.jsx
+++ FirstReactApp/src/components/Info.jsx
@@ -1,15 +1,32 @@
-import profileImage from '../images/profile_pic.png';
+import './Info.css';
+<Info className="css"></Info>;
+import profileImage from '../images/profile_pic.jpg';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
 
 function Info() {
   return (
-    <div>
-      <img src={profileImage} alt="Profile picture" width="317px" height="317px"></img>
-      <h1>Maximilian Stadlmeyer</h1>
-      <h2>Frontend Developer</h2>
-      <div>
-        <a href="#">maxstadlmeyer.dev</a>
+    <div className="info-container">
+      <img
+        className="profile-pic"
+        src={profileImage}
+        alt="Profile picture"
+        width="317px"
+        height="317px"
+      ></img>
+      <div className="contact-container">
+        <h1 className="name">Maximilian Stadlmeyer</h1>
+        <h2 className="job">Frontend Developer</h2>
+        <a className="website-link" href="#">
+          maxstadlmeyer.dev
+        </a>
+        <a className="email-link" href="#">
+          <div className="email-btn">
+            <FontAwesomeIcon className="email-icon" icon={faEnvelope} />
+            Email
+          </div>
+        </a>
       </div>
-      <a href="#">Email</a>
     </div>
   );
 }
